ce4e8c9e2113f351b369be6bf4bd650cfcfbb412
KucharallapatiAkashV <akashvarma.k@gmail.com>
2026-07-07 19:47:42 +0530

    Useeffect mount and unmount with example

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import './App.css'
 import { useEffect, useState } from 'react';
+import ParentComponent from './assets/ParentComponent';
 
 function App() {
-
   
-const [count, setCount] = useState(0);
-const [anotherValue, setAnotherValue] = useState(10);
 
-useEffect(() => {
-  console.log('useEffect Triggered')
-}, [count]);
-
-const incrementCount = () => {
-  setCount(count + 1);
-};
 
   return (
 
     <div>
-      <h1>useEffect</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setAnotherValue(anotherValue + 1)}>Another Value</button>
-
-
+     <ParentComponent/>
     </div>
 
   )
