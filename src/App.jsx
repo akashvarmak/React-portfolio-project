093cde86b32eacf39c6f90e69948007562df83b0
KucharallapatiAkashV <akashvarma.k@gmail.com>
2026-07-10 20:38:44 +0530

    very important html input and useref link concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navigation from './assets/Components/navigation'
+import { useRef } from 'react';
 import './App.css'
 
 //useRef
@@ -8,18 +8,43 @@ import './App.css'
 //current is the variable inside the object of useRef, gpt this
 
 function App() {
+  const inputRef = useRef(null);
+  const inputRefNext = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'yellow';
+
+  }
+
+  const focusInputNext = () => {
+    inputRefNext.current.focus();
+    inputRefNext.current.style.backgroundColor = 'yellow';
+
+  }
+
+   const resetFocus = () => {
+    inputRefNext.current.style.backgroundColor = 'white';
+    inputRef.current.style.backgroundColor = 'white';
 
 
+  }
 
 
   return (
 
     <div>
+      <h1>Learn React</h1>
+      <input type='text' ref={inputRef} plaveholder='Focus me'></input>
+      <button onClick={focusInput}>Focus and Highlight</button>
+
+      <input type='text' ref={inputRefNext} plaveholder='Focus me'></input>
+      <button onClick={focusInputNext}>Focus and Highlight</button>
+
+      <button onClick={resetFocus}>reset</button>
+
+
       
-      <Navigation />
-
-
-
     </div>
 
   )
