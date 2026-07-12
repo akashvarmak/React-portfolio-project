8cc67bcd0e0da35233b5742b8bbdceec6b87762f
KucharallapatiAkashV <akashvarma.k@gmail.com>
2026-07-12 18:11:04 +0530

    custom-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,21 @@
-import { createContext, useContext, useRef, useState } from 'react';
+import React from 'react';
 import './App.css'
-
-//useContext()
-
-//create a constant
-//const MyContext = createContext(defaultValue);
-
-//consume a context:
-//const contextValue = useContext(MyContext);
-
-// <MyContext.Provider value={somevalue}>
-//       <ComponentA/>
-//   </MyContext.provider>
-
- const ThemeContext = createContext('light');
-
+import useCounter from './useCounter';
 
 function App() {
-  const [theme, setTheme] = useState('light');
-
-  const toggleTheme = () => {
-      setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
+  const {count, increment, decrement} = useCounter()
+  
   return (
+
     <div>
-      //defualt outside provider
-     <GlobalComponent/>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
-    //Hirarchy of internal components change
-    <ThemeContext.Provider value={theme}>
-    <div style={{border:'2px solid black', padding: '20px' }}>
-    <h2>App (parent)</h2>
-    <button onClick={toggleTheme}>Toggle Theme</button>
-    <ComponentA/>
-    </div>
-    </ThemeContext.Provider>
-
-    // provider with our custom value
-    <ThemeContext.Provider value='dark'>
-      <GlobalComponent/>
-    </ThemeContext.Provider>
 
     </div>
-
-  );
+  );  
+}
 
   
-}
-
-
-function ComponentA() {
-  return (
-    <div style={{border:'2px solid black', padding: '20px' }}>
-    <h2>ComponentA (child)</h2>
-    <ComponentB />
-
-    </div>
-
-  );
-}
-
-function ComponentB() {
-   return (
-    <div style={{border:'2px solid black', padding: '20px' }}>
-    <h2>ComponentB (Grand-child)</h2>
-    <ThemedComponent/>
-
-    </div>
-
-  );
-}
-
-function ThemedComponent() {
-  const theme = useContext(ThemeContext);
-   return (
-    <div style={{border:'2px solid black', padding: '20px' }}>
-    <h2>ThemedComponent (Great-Grand-child)</h2>
-    <div>The current theme is: {theme}</div>
-
-    </div>
-
-  );
-  
-}
-
-
-function GlobalComponent() {
-  const theme = useContext(ThemeContext);
-   return (
-    <div style={{border:'2px solid purple', padding: '20px' }}>
-    <h2>GlobalComponent (outside provider)</h2>
-    <div>The current theme is: {theme}</div>
-
-    </div>
-
-  );
-  
-}
-
 export default App
