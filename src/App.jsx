9ef536eef5fa40af8904723c779d0b24e370effd
KucharallapatiAkashV <akashvarma.k@gmail.com>
2026-07-16 11:45:59 +0530

    API Lec - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
-import useCounter from './useCounter';
+
 
 function App() {
-  const {count, increment, decrement, reset} = useCounter(0)
+
+  const [data, setData] = useState([]);
+ 
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
   
   return (
 
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+
+       <h2>API's</h2>
+       <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <p><strong>{post.title}</strong></p>
+            <p>{post.body}</p>
+
+
+          </li>
+          ))}
+       </ul>
+       
+
+     
 
 
     </div>
