a55f7123326bfd47e9e3bd8a27ea00bdade11b5d
KucharallapatiAkashV <akashvarma.k@gmail.com>
2026-07-18 20:38:17 +0530

    fetch call

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,33 @@ import './App.css'
 function App() {
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
  
   useEffect( () => {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => {
+        setData(json);
+        setLoading(false);
+        //  throw new Error('Something went wrong');
+      })
+      .catch((error) => {
+        //console.error gives us in red
+        console.error(`Error fetching data: `, error);
+        setError(`Failed to fetch the data`);
+        setLoading(false);
+      });
   }, []);
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
+  if(error) {
+    return <p>{error}</p>
+  }
   
   return (
 
